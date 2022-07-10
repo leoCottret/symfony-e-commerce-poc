cd21ec5d7408db5d760dafdedc64e25d69bf522a
leoCottret <leocottret@protonmail.com>
2022-07-10 20:40:19 +0200

    [FEATURE] add products

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+private $em;
+
+public function __construct(EntityManagerInterface $em)
+{
+    $this->em = $em;
+}
+
+
+    /**
+     * @Route("/products", name="app_product")
+     */
+    public function index(): Response
+    {
+        $products = $this->em->getRepository(Product::class)->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
